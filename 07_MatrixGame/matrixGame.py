bac58344802fc9f0efbdda46a3b4fc72a72b6e19
Lazar <celikoviclazar@hotmail.com>
2021-04-10 21:26:32 +0200

    Implement dimension reducing

--- 07_MatrixGame/matrixGame.py
+++ 07_MatrixGame/matrixGame.py
@@ -75,7 +75,7 @@ def getDomCols(matrix):
             col2 = extractColumn(matrix, j)
 
             if compareVectors(col1, col2):
-                return j
+                return i
 
     return None
 
@@ -139,8 +139,8 @@ def reduce_dimension(matrix):
 
     return matrix
 
-#matrix = np.array([[2, 1, 2, 3], [3, 1.5, 1, 2], [2, 2, 1, 1], [1, 1, 1, 1]])
-matrix = np.array([[1, -1, -1], [-1, -1, 3], [-1, -2, -1]])
+matrix = np.array([[2, 1, 2, 3], [3, 1.5, 1, 2], [2, 2, 1, 1], [1, 1, 1, 1]])
+#matrix = np.array([[1, -1, -1], [-1, -1, 3], [-1, -2, -1]])
 matrix = reduce_dimension(matrix)
 print(matrix)
 
