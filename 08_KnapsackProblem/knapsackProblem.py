e2f1f657171b4e908776e03876e170ae4cb7b31d
Lazar <celikoviclazar@hotmail.com>
2021-04-19 20:22:45 +0200

    Implement unbounded knapsack problem

--- 08_KnapsackProblem/knapsackProblem.py
+++ 08_KnapsackProblem/knapsackProblem.py
@@ -1,3 +1,5 @@
+import math
+
 import numpy as np
 
 def read_input():
@@ -22,7 +24,7 @@ def read_input():
     return numOfArticles, backpackVolume, prices, volumes
 
 
-def reconstruct_result(memo, volumes):
+def reconstruct_result_0_1(memo, volumes):
 
     res = list()
 
@@ -43,9 +45,107 @@ def reconstruct_result(memo, volumes):
 
     res.reverse()
     return res
+# Vrsi rekonstrukciju resenja za problem ranca
+# u kom mozemo ubacivati vise puta isti predmet
+def reconstruct_result(memo, volumes, prices, numOfArticles, backpackVolume):
+
+    vec = np.zeros(numOfArticles)
+
+    n = len(memo) - 1
+    m = len(memo[0]) - 1
+
+    price = memo[numOfArticles - 1][backpackVolume]
+
+    while (n > 0):
+        if memo[n][m] != memo[n - 1][m]:
+            vec[n] += 1
+            price -= prices[n]
+            m -= volumes[n]
+        else:
+            n -= 1
+
+    if price > 0:
+        vec[0] += math.floor(price / prices[0])
+
+    return vec
 
 
-def knapsack_in_advance(numOfArticles, backpackVolume, volumes, prices):
+def maximum(vec):
+
+    if len(vec) == 0:
+        return 0
+
+    maxi = float('-inf')
+    for x in vec:
+        if x > maxi:
+            maxi = x
+    return int(maxi)
+
+# Problem ranca u nazad
+def UnboundedKnapsack(numOfArticles, backpackVolume, volumes, prices):
+
+    memo = [[0 for i in range(backpackVolume + 1)] for i in range(numOfArticles)]
+
+    for i in range(len(memo[0])):
+        memo[0][i] = prices[0] * (math.floor(i / volumes[0]))
+
+    for i in range(1, len(memo)):
+        for j in range(len(memo[0])):
+
+            k = math.floor(j / volumes[i])
+            vec = []
+            for ind in range(k+1):
+                vec.append(ind*prices[i] + memo[i-1][j - volumes[i]*ind])
+
+            memo[i][j] = maximum(vec)
+
+    vec = reconstruct_result(memo, volumes, prices, numOfArticles, backpackVolume)
+
+    return memo[numOfArticles-1][backpackVolume], memo, vec
+
+
+
+# Problem ranca u napred
+def UnboundedKnapsackInAdvance(numOfArticles, backpackVolume, volumes, prices):
+
+    memo = [[0 for i in range(backpackVolume+1)] for i in range(numOfArticles)]
+
+    for i in range(len(memo[0])):
+        memo[0][i] = prices[0]*(math.floor(i / volumes[0]))
+
+    for i in range(1, len(memo)):
+        for j in range(len(memo[0])):
+
+            if volumes[i] <= j:
+                memo[i][j] = max(memo[i-1][j], memo[i][j - volumes[i]] + prices[i])
+            else:
+                memo[i][j] = memo[i-1][j]
+
+    # Sada prelazimo na rekonstrukciju
+
+    vec = reconstruct_result(memo, volumes, prices, numOfArticles, backpackVolume)
+
+    return memo[numOfArticles-1][backpackVolume], memo, vec
+
+# U ovoj implementaciji moguce je uzeti proizvoljan broj
+# istih elemenata i ubaciti ih u ranac
+def UnboundedKnapsackOptimized(backpackVolume, volumes, prices):
+
+    memo = [0 for i in range(backpackVolume+1)]
+    start = min(volumes)
+
+    for i in range(start, backpackVolume+1):
+        memo[i] = memo[i-1]
+        for k, vk in enumerate(prices):
+            if volumes[k] <= i and memo[i] < memo[i - volumes[k]] + vk:
+                memo[i] = memo[i - volumes[k]] + vk
+
+    return memo[backpackVolume], memo
+
+
+# U ovoj implementaciji je moguce svaki predmet uzeti najvise
+# jednom i ubaciti ga u ranac
+def knapsack_0_1(numOfArticles, backpackVolume, volumes, prices):
     memo = np.zeros((numOfArticles+1, backpackVolume+1))
 
     for i in range(numOfArticles + 1):
@@ -59,7 +159,7 @@ def knapsack_in_advance(numOfArticles, backpackVolume, volumes, prices):
 
     # Sada imamo tablicu i vrednost
     # Potrebno je rekonstruisati resenje
-    vector = reconstruct_result(memo, volumes)
+    vector = reconstruct_result_0_1(memo, volumes)
     result = [0] * numOfArticles
     for val in vector:
         result[val] = 1
@@ -71,6 +171,10 @@ def knapsack_in_advance(numOfArticles, backpackVolume, volumes, prices):
 
 
 numOfArticles, backpackVolume, prices, volumes = read_input()
-
-# Ranac unapred
-memo, res = knapsack_in_advance(numOfArticles, backpackVolume, volumes, prices)
+res, memo, vec = UnboundedKnapsackInAdvance(numOfArticles, backpackVolume, volumes, prices)
+print(f"Optimalno resenje je = {res}")
+print(f"Optimalna tacka je = {vec}")
+print("----------------------------------------------------")
+res, memo, vec = UnboundedKnapsack(numOfArticles, backpackVolume, volumes, prices)
+print(f"Optimalno resenje je = {res}")
+print(f"Optimalna tacka je = {vec}")
