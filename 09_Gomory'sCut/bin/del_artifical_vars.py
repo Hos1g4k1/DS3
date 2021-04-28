02161b39e77256508964297478cd3edddda5c464
Lazar <celikoviclazar@hotmail.com>
2021-04-28 19:56:56 +0200

    Implemented Gomory's cutting plane method for integer programming

---
+++ 09_Gomory'sCut/bin/del_artifical_vars.py
@@ -0,0 +1,93 @@
+import random
+import math
+import numpy as np
+
+def del_artifical_vars(table, B, W):
+    use_bland = True
+    print_steps = False
+    while len(W)!=0:
+        # Removing aritficial variables:
+        # If they are not in the base we cross them over
+        non_basis = [i for i in W if i not in B]
+        if print_steps:
+            print()
+            print('Table before removing base variables')
+            for r in table:
+                print([round(i, 2) for i in r])
+            print()
+            print('Removing aritificial, non basis, vars')
+            print('non_basis = {}'.format(non_basis))
+        for i in range(len(table)):
+            table[i] = [value for index,value in enumerate(table[i]) if index not in non_basis ]
+        # Uprading artifiicial var indexes
+        # If we removed any variable that was smaller than w we have to remove it's index too so that
+        # it can correspond to the table W
+        W = [i for i in W if i not in non_basis]
+        for i in range(len(W)):
+            smaller_num = len([j for j in non_basis if j <W[i]])
+            W[i] -=smaller_num
+
+        for i in range(len(B)):
+            smaller_num = len([j for j in non_basis if j <B[i]])
+            B[i] -=smaller_num
+
+        if print_steps:
+            print('Table after removing non base variables')
+            for r in table:
+                print([round(i, 2) for i in r])
+            print()
+            print('Removing artificial base variables')
+            print('Artificial base variables = {}'.format(W))
+        # Removing all artificial base variables
+        # If all are 0 in one row except 1, then we remove that artificial variable and we remove both the row
+        # and the corresponding column 
+        # Else if such an element exists that is different that 0, we choose it and make base
+        # after that, since that artificial variable is no longer in base we remove it just like before
+        removed = []
+        for w in W:
+            # row = B.index(w)
+            row = np.where(B == w)[0].item()
+            indexes = [i for i,j in enumerate(table[row][:-1]) if i!=w]
+            nonzero = any(table[row][i]!=0 for i in indexes)
+
+            if nonzero:
+                # If there is an element that is not 0 we take any of them and make it a base by pivoting
+                candidate = [i for i in indexes if table[row][i]!=0]
+                ind = candidate[0]
+                if not use_bland:
+                    ind = random.choice(candidate)
+
+                table[row] = [i/table[row][ind] for i in table[row]]
+
+                for i in range(len(table)):
+                    if i!=row:
+                        mult = -table[i][ind]
+                        table[i] = [i+mult*j for i,j in zip(table[i],table[row])]
+                B[row] = ind
+                if print_steps:
+                    print('Since artificial variable x{} has an el!=0 in the same row, we pivot around A[{}][{}]'.format(w, row, ind))
+
+                    print('New base is: {}'.format(B))
+                    print()
+            else:
+                # If all are 0 then we delte both the row and the column
+                table.pop(row)
+                for i in range(len(table)):
+                    table[i].pop(w)
+
+                B = B.tolist()
+                el = B.pop(row)
+
+                B = [i if el>i else i-1 for i in B]
+                W.remove(w)
+                W = [i if el > i else i - 1 for i in W]
+                B = np.array(B)
+                if print_steps:
+                    print('Since artificial variable x{} is in a row where the base is and all are = 0 we delete the row and the column'.format(w))
+            if print_steps:
+                print()
+                print('Table after removing base variables')
+                for r in table:
+                    print([round(i, 2) for i in r])
+
+    return table,B
---
+++ 09_Gomory'sCut/main.py
@@ -0,0 +1,156 @@
+import numpy as np
+
+from bin.classes.class_system import *
+from bin.input import input_vars
+from bin.revised_simplex_with_ETA import revised_simplex_with_ETA
+from bin.canonical_form import canonical_form
+from bin.make_standardized_form import make_standardized_form
+from bin.print_ import *
+from bin.dual_simplex import dual_simplex
+from bin.two_phase_simplex import *
+import copy
+
+def decompose(table):
+
+    n = len(table)
+    m = len(table[0])
+
+    A = np.zeros((n-1, m-1))
+    c = np.zeros(m-1)
+    b = np.zeros(n-1)
+    F = -1
+
+    for i in range(n):
+        for j in range(m):
+            if i < n-1 and j < m-1:
+                A[i][j] = table[i][j]
+            elif i == n-1 and j < m-1:
+                c[j] = table[i][j]
+            elif i < n-1 and j == m-1:
+                b[i] = table[i][j]
+            else:
+                F = table[i][j]
+
+    return A, b, c, F
+
+def isclose(a, b, rel_tol=1e-09, abs_tol=0.0):
+    return abs(a-b) <= max(rel_tol * max(abs(a), abs(b)), abs_tol)
+
+def isInteger(vec):
+
+    vec = np.round(vec, 8)
+
+    for i in range(len(vec)):
+        if not vec[i].is_integer():
+            return False
+
+    return True
+
+def findFirstNonInt(vec):
+
+    for i in range(len(vec)):
+        if not vec[i].is_integer():
+            return i
+
+def makeNewConstr(vec, F):
+
+    new_vec = np.zeros(len(vec))
+
+    for i in range(len(vec)):
+        if vec[i] >= 0:
+            new_vec[i] = vec[i] - int(vec[i])
+        else:
+            val = int(abs(vec[i]))+1
+            new_vec[i] = val + vec[i]
+
+    if F >= 0:
+        F = F - int(F)
+    else:
+        F = int(abs(F)) + 1 + F
+
+    return new_vec, F
+
+def addSlack(tab):
+
+    tab = tab.tolist()
+
+    for x in tab:
+        x.append(0)
+
+    return tab
+
+def calculateSol(c, vec):
+
+    F = 0
+
+    for i in range(len(c)):
+        F += c[i] * vec[i]
+
+    return -F
+
+def printCut(constr, F):
+
+    print("=========================================")
+    print("Cut:")
+    print(f"{np.round(constr, 8)} = {np.round(F, 8)}")
+    print("=========================================")
+
+def main():
+    np.set_printoptions(formatter={'float': lambda x: "{0:0.3f}".format(x)})
+
+    # exit()
+    n, m, otype, in_c, in_A, sign, in_b = input_vars()
+    varNum = len(in_A[0])
+    s = System([n, m, otype, in_c, in_A, sign, in_b])
+    s.print_problem()
+    oproblem = s.problem
+    make_standardized_form(s)
+    table,x = two_phase_simplex(s, problem=oproblem)
+
+    A, b, c, F = decompose(table)
+    first_c = copy.deepcopy(in_c)
+
+    val = -1
+    tab = np.vstack((c, A))
+    vec = np.hstack((F, b))
+
+    while not isInteger(vec):
+
+        ind = findFirstNonInt(vec)
+
+        newConstr, F = makeNewConstr(tab[ind], vec[ind])
+        newConstr = -newConstr
+        newConstr = np.hstack((newConstr, np.array([1])))
+
+        printCut(newConstr, -F)
+
+        A = addSlack(A)
+        A = np.vstack((A, newConstr))
+        b = np.hstack((b, -F))
+        c = np.hstack((c, 0))
+
+        signs = list()
+        for i in range(len(A)):
+            signs.append("=")
+
+        s = System([len(A), len(A[0]), 'min', c, A, signs, b])
+        s.print_problem()
+        oproblem = s.problem
+        make_standardized_form(s)
+        table, x = two_phase_simplex(s, problem=oproblem)
+
+        A, b, c, F = decompose(table)
+
+        tab = np.vstack((c, A))
+        val = calculateSol(first_c, x)
+        vec = np.hstack((val, b))
+
+    finalSol = list()
+    for i in range(varNum):
+        finalSol.append(x[i])
+
+    print(f"Solution: {np.round(finalSol, 8)}")
+    print(f"Optimal value: {-val}")
+
+if __name__ == '__main__':
+   main()
